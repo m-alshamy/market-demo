a641868444b36a154b9b7cb353511c5b34505a3c
m-alshamy <elshamiatl@gmail.com>
2026-09-22 09:35:04 +0300

    b to balance

--- public/home/wallet.js
+++ public/home/wallet.js
@@ -7,7 +7,7 @@ async function fetchWalletBalance() {
 
     const { data, error } = await supabaseClient
         .from('wallet')
-        .select('b')
+        .select('balance')
         .eq('user_id', session.user.id)
         .single();
 
